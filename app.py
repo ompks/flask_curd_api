5916c87d98265a9eaf0859d4a229c50ad6a70942
Hashi <kumarnisit@gmail.com>
2020-01-24 16:49:26 +0530

    added reqparse,removed f-strings and changed secret key

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 from flask import Flask, request
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 from flask_jwt import JWT, jwt_required
 from security import authenticate, identity
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'super-secret-key'
+app.config['SECRET_KEY'] = 'f3ff4158c7fc7ac1086f284e568143b6a97249508998c0b3474541d17bbe74ccba891f048e98e33a375dd714'
 api = Api(app)
 jwt = JWT(app, authenticate, identity)
 items = []
 
 class Item(Resource):
+
+	parser = reqparse.RequestParser()
+	parser.add_argument('price',
+			type=float,
+			required=True,
+			help='This field can not be left blank!')
 
 	def get(self, name):
 		item = next(filter(lambda x: x['name'] == name, items), None)
@@ -18,7 +24,7 @@ class Item(Resource):
 	def post(self, name):
 		if next(filter(lambda x: x['name'] == name, items), None):
 			return {'message': 'Item already exists!'}, 400
-		data = request.get_json()
+		data = Item.parser.parse_args()
 		new_item = {'name': name, 'price': data['price']}
 		items.append(new_item)
 		return new_item, 201
@@ -26,16 +32,16 @@ class Item(Resource):
 	def delete(self, name):
 		global items
 		items = list(filter(lambda x: x['name'] != name, items))
-		return {f'message': 'Item {name} has been deleted!'}
+		return {'message': 'Item has been deleted!'}
 
 	def put(self, name):
 		item = next(filter(lambda x: x['name'] == name, items), None)
 		if item:
-			data = request.get_json()
+			data = Item.parser.parse_args()
 			item['price'] = data['price']
-			return {f'message': 'Item {name} has been updated!'}
+			return {'message': 'Item has been updated!'}
 		else:
-			return {f'message': 'Item {name} does not exist!'}, 400
+			return {'message': 'Item does not exist!'}, 400
 		
 
 class Items(Resource):
